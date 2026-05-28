e5b1b91ca99c97b4b2eb0e9ec462a652045a6ea4
OnHyex <100549656+OnHyex@users.noreply.github.com>
2026-05-27 22:22:49 -0400

    Preliminary addition of the abstract component manager + legacy type compatibility builder

---
+++ PulsarModLoader/Content/Components/AbstractComponentManager.cs
@@ -0,0 +1,235 @@
+﻿#nullable enable
+using PulsarModLoader.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PulsarModLoader.Content.Components
+{
+    public class ComponentReflectionConstructor<TComp> where TComp : PLShipComponent
+    {
+        public TComp CreateWithThreeArgsExplicit(Type specificComponentType, object arg1, object arg2, object arg3)
+        {
+            if (!ReflectionCache.TryGetValue(specificComponentType, out var constructor))
+            {
+                // Define the expected types of your three arguments
+                var argTypes = new[] { arg1.GetType(), arg2.GetType(), arg3.GetType() };
+
+                // Find the public constructor
+                constructor = specificComponentType.GetConstructor(argTypes);
+
+                if (constructor == null)
+                {
+                    throw new Exception($"3 Parameter Constructor not found for type {specificComponentType.Name}");
+                }
+
+                ReflectionCache.TryAdd(specificComponentType, constructor);
+            }
+            TComp comp = (TComp)constructor.Invoke(new[] { arg1, arg2, arg3 });
+            if (comp is not null)
+            {
+                return comp;
+            }
+            throw new Exception($"Constructor failed for {specificComponentType.Name}");
+        }
+        protected readonly Dictionary<Type, ConstructorInfo> ReflectionCache = new Dictionary<Type, ConstructorInfo>();
+    }
+    public abstract class ComponentModManager<TComp> where TComp : PLShipComponent
+    {
+        public readonly int VanillaMaxType = 0;
+        private readonly int _MainType = 0;
+        protected readonly Dictionary<PulsarMod, List<Type>> componentsByMod = new Dictionary<PulsarMod, List<Type>>();
+        protected readonly List<Type> components = new List<Type>(32);
+        protected internal ComponentModManager(int MainType)
+        {
+            _MainType = MainType;
+            foreach (PulsarMod mod in ModManager.Instance.GetAllMods())
+            {
+                Assembly asm = mod.GetType().Assembly;
+                foreach (Type t in asm.GetTypes())
+                {
+                    if (typeof(TComp).IsAssignableFrom(t) && !t.IsInterface && !t.IsAbstract)
+                    {
+                        if (componentsByMod.ContainsKey(mod))
+                        {
+                            componentsByMod[mod].Add(t);
+                        }
+                        else
+                        {
+                            componentsByMod[mod] = new List<Type>() { t };
+                        }
+                    }
+                }
+            }
+        }
+        protected internal ComponentModManager(int MainType, int MaxType) : this(MainType)
+        {
+            VanillaMaxType = MaxType;
+        }
+        protected readonly ComponentReflectionConstructor<TComp> constructorFactory = new ComponentReflectionConstructor<TComp>();
+        protected virtual TComp? CreateComponent(Type compType, int SubType, int Level, int SubTypeData)
+        {
+            try
+            {
+                return constructorFactory.CreateWithThreeArgsExplicit(compType, SubType, Level, (short)SubTypeData);
+            }
+            catch (Exception ex)
+            {
+                Logger.Info($"Failed to create modded component {compType.Name}, {ex}");
+                return (TComp)PLShipComponent.CreateShipComponentFromHash((int)PLShipComponent.createHashFromInfo(this._MainType, this.VanillaMaxType, Level, SubTypeData, (int)ESlotType.E_COMP_CARGO));
+            }
+        }
+        protected virtual void HandleModUnLoaded(PulsarMod? mod)
+        {
+            if (mod is not null && componentsByMod.ContainsKey(mod))
+            {
+                componentsByMod.Remove(mod);
+                components.Clear();
+                foreach (var kvp in componentsByMod)
+                {
+                    foreach (Type type in kvp.Value)
+                    {
+                        components.Add(type);
+                    }
+                }
+            }
+        }
+        public int GetIDFromName(string name)
+        {
+            for (int i = 0; i < components.Count; i++)
+            {
+                if (components[i].Name == name)
+                {
+                    return i + VanillaMaxType;
+                }
+            }
+            return -1;
+        }
+        public int GetIDFromType(Type type)
+        {
+            for (int i = 0; i < components.Count; i++)
+            {
+                if (components[i].GetType() == type || (components[i] is ILegacyComponent legacy && legacy.GetLegacyType() == type))
+                {
+                    return i + VanillaMaxType;
+                }
+            }
+            return -1;
+        }
+
+        public IReadOnlyList<Type> ModTypes
+        {
+            get
+            {
+                return components.AsReadOnly();
+            }
+        }
+    }
+    public abstract class ComponentModManager<TComp,TEnum> : ComponentModManager<TComp> where TComp : PLShipComponent where TEnum : Enum
+    {
+        protected ComponentModManager() : base (Enum.GetValues(typeof(TEnum)).Length)
+        {
+            Logger.Info($"{typeof(TComp).Name} MaxTypeint: {VanillaMaxType - 1}");
+        }
+    }
+    public abstract class ComponentModManager<TComp,TLegacyModComp,TEnum> : ComponentModManager<TComp> where TComp : PLShipComponent where TLegacyModComp : ComponentModBase where TEnum : Enum 
+    {
+        protected readonly LegacyComponentCompatabilityFactory<TComp, TLegacyModComp> legacyComponentFactory;
+
+        protected ComponentModManager() : base (Enum.GetValues(typeof(TEnum)).Length)
+        {
+            Logger.Info($"{typeof(TComp).Name} MaxTypeint: {VanillaMaxType - 1}");
+
+            legacyComponentFactory = new LegacyComponentCompatabilityFactory<TComp, TLegacyModComp>(typeof(TEnum), (ILGenerator il) => { BaseClassConstructor(il); }, (TypeBuilder builder, FieldBuilder field) => { ModComponentSubtypeMethods(builder, field); });
+            foreach (PulsarMod mod in ModManager.Instance.GetAllMods())
+            {
+                Assembly asm = mod.GetType().Assembly;
+                foreach (Type t in asm.GetTypes())
+                {
+                    if (typeof(TLegacyModComp).IsAssignableFrom(t) && !t.IsInterface && !t.IsAbstract)
+                    {
+                        //Logger.Info($"Loading {typeof(TMod).Name} from assembly");
+                        TLegacyModComp handler = (TLegacyModComp)Activator.CreateInstance(t);
+                        if (GetIDFromName(handler.Name) == -1)
+                        {
+                            try
+                            {
+                                Type generatedType = legacyComponentFactory.DefineNewType(handler);
+                                if (componentsByMod.ContainsKey(mod))
+                                {
+                                    componentsByMod[mod].Add(generatedType);
+                                }
+                                else
+                                {
+                                    componentsByMod[mod] = new List<Type>() { generatedType };
+                                }
+                            }
+                            catch (Exception ex)
+                            {
+                                Logger.Info($"Could not add legacy component {typeof(TLegacyModComp).Name} from {mod.Name} due to error {ex.ToString()}, StackTrace: {Environment.StackTrace}");
+                            }
+
+                            Logger.Info($"Added legacy component {typeof(TLegacyModComp).Name}: '{handler.Name}' with ID '{GetIDFromName(handler.Name)}' to dynamic builder");
+                        }
+                        else
+                        {
+                            Logger.Info($"Could not add legacy component {typeof(TLegacyModComp).Name} from {mod.Name} with the duplicate name of '{handler.Name}' to dynamic builder");
+                        }
+                    }
+                }
+            }
+        }
+        protected virtual void ComponentModConstructor(TComp comp, ComponentModBase legacyComp, int subType, int level, short subTypeData)
+        {
+            comp.Name = legacyComp.Name;
+            comp.Desc = legacyComp.Description;
+            comp.m_MarketPrice = legacyComp.MarketPrice;
+            comp.CargoVisualPrefabID = legacyComp.CargoVisualID;
+            comp.CanBeDroppedOnShipDeath = legacyComp.CanBeDroppedOnShipDeath;
+            comp.Experimental = legacyComp.Experimental;
+            comp.Unstable = legacyComp.Unstable;
+            comp.Contraband = legacyComp.Contraband;
+            comp.Price_LevelMultiplierExponent = legacyComp.Price_LevelMultiplierExponent;
+            comp.m_IconTexture = legacyComp.IconTexture;
+        }
+        protected abstract void BaseClassConstructor(ILGenerator il);
+        protected abstract void ModComponentSubtypeMethods(TypeBuilder builder, FieldBuilder legacyField);
+        protected override TComp? CreateComponent(Type compType, int SubType, int Level, int SubTypeData)
+        {
+            TComp? comp = base.CreateComponent(compType, SubType, Level, SubTypeData);
+            if (comp is ILegacyComponent LegacyComp)
+            {
+                ComponentModConstructor(comp, LegacyComp.GetComponentMod(), SubType, Level, (short)SubTypeData);
+            }
+            return comp;
+        }
+        public readonly List<TLegacyModComp?> legacyModComps = new List<TLegacyModComp?>();
+        protected override void HandleModUnLoaded(PulsarMod? mod)
+        {
+            base.HandleModUnLoaded(mod);
+            legacyModComps.Clear();
+            foreach (var type in components)
+            {
+                if (typeof(ILegacyComponent).IsAssignableFrom(type))
+                {
+                    FieldInfo legacyField = type.GetField("_legacy", BindingFlags.Static | BindingFlags.NonPublic);
+                    legacyModComps.Add((TLegacyModComp)legacyField.GetValue(null));
+                }
+                else
+                {
+                    legacyModComps.Add(null);
+                }
+            }
+        }
+    }
+    public interface ILegacyComponent
+    {
+        public Type GetLegacyType();
+        public ComponentModBase GetComponentMod();
+    }
+}
---
+++ PulsarModLoader/Content/Components/LegacyComponentCompatabilityFactory.cs
@@ -0,0 +1,220 @@
+﻿#nullable enable
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PulsarModLoader.Content.Components
+{
+    public class LegacyComponentCompatabilityFactory<TComp, TLegacyComp> where TComp : PLShipComponent where TLegacyComp : ComponentModBase
+    {
+        
+
+        private readonly Action<ILGenerator> componentBaseConstructorBuilder;
+        private readonly Action<TypeBuilder,FieldBuilder> nonComponentBaseMethodAddition;
+        private readonly AssemblyBuilder asm;
+        private readonly ModuleBuilder module;
+        private readonly Type? baseEnumType;
+        public LegacyComponentCompatabilityFactory(Type? Enum, Action<ILGenerator> componentBaseConstructorBuilder, Action<TypeBuilder, FieldBuilder> nonComponentBaseMethodAddition)
+        {
+            this.componentBaseConstructorBuilder = componentBaseConstructorBuilder;
+            this.nonComponentBaseMethodAddition = nonComponentBaseMethodAddition;
+            this.baseEnumType = Enum;
+
+            var asmName = new AssemblyName($"{typeof(TComp).Name}" + "DynamicAssembly");
+
+            asm = AssemblyBuilder.DefineDynamicAssembly(asmName, AssemblyBuilderAccess.Run);
+
+            module = asm.DefineDynamicModule("MainModule");
+        }
+        private static readonly ReadOnlyDictionary<MethodInfo, MethodInfo> componentModBaseMethodOverrides;
+        private static readonly MethodInfo objectGetType;
+    
+        static LegacyComponentCompatabilityFactory()
+        {
+            objectGetType = typeof(object).GetMethod(nameof(GetType));
+            componentModBaseMethodOverrides = new ReadOnlyDictionary<MethodInfo, MethodInfo>(new Dictionary<MethodInfo, MethodInfo>()
+                {
+                    {
+                    // Forward Legacy AddStats to PLShipStats.AddStats()
+                        typeof(PLShipComponent).GetMethod(nameof(PLShipComponent.AddStats), BindingFlags.Instance | BindingFlags.Public | BindingFlags.DeclaredOnly),
+                        typeof(ComponentModBase).GetMethod(nameof(ComponentModBase.AddStats), BindingFlags.Instance | BindingFlags.Public | BindingFlags.DeclaredOnly)
+                    },
+                    {
+                    // Forward Legacy FinalLateAddStats to PLShipStats.FinalLateAddStats()
+                        typeof(PLShipComponent).GetMethod(nameof(PLShipComponent.FinalLateAddStats), BindingFlags.Instance | BindingFlags.Public | BindingFlags.DeclaredOnly),
+                        typeof(ComponentModBase).GetMethod(nameof(ComponentModBase.FinalLateAddStats), BindingFlags.Instance | BindingFlags.Public | BindingFlags.DeclaredOnly)
+                    },
+                    {
+                    // Forward Legacy LateAddStats to PLShipStats.LateAddStats()
+                        typeof(PLShipComponent).GetMethod(nameof(PLShipComponent.LateAddStats), BindingFlags.Instance | BindingFlags.Public | BindingFlags.DeclaredOnly),
+                        typeof(ComponentModBase).GetMethod(nameof(ComponentModBase.LateAddStats), BindingFlags.Instance | BindingFlags.Public | BindingFlags.DeclaredOnly)
+                    },
+                    {
+                    // Forward Legacy GetStatLineRight to PLWare.GetStatLineRight()
+                        typeof(PLShipComponent).GetMethod(nameof(PLWare.GetStatLineRight), BindingFlags.Instance | BindingFlags.Public | BindingFlags.DeclaredOnly),
+                        typeof(ComponentModBase).GetMethod(nameof(ComponentModBase.GetStatLineRight), BindingFlags.Instance | BindingFlags.Public | BindingFlags.DeclaredOnly)
+                    },
+                    {
+                    // Forward Legacy GetStatLineLeft to PLWare.GetStatLineLeft()
+                        typeof(PLShipComponent).GetMethod(nameof(PLWare.GetStatLineLeft), BindingFlags.Instance | BindingFlags.Public | BindingFlags.DeclaredOnly),
+                        typeof(ComponentModBase).GetMethod(nameof(ComponentModBase.GetStatLineLeft), BindingFlags.Instance | BindingFlags.Public | BindingFlags.DeclaredOnly)
+                    },
+                    {
+                    // Forward Legacy OnWarp to PLShipComponent.OnWarp()
+                        typeof(PLShipComponent).GetMethod(nameof(PLShipComponent.OnWarp), BindingFlags.Instance | BindingFlags.Public | BindingFlags.DeclaredOnly),
+                        typeof(ComponentModBase).GetMethod(nameof(ComponentModBase.OnWarp), BindingFlags.Instance | BindingFlags.Public | BindingFlags.DeclaredOnly)
+                    },
+                    {
+                    // Forward Legacy Tick to PLShipComponent.Tick()
+                        typeof(PLShipComponent).GetMethod(nameof(PLShipComponent.Tick), BindingFlags.Instance | BindingFlags.Public | BindingFlags.DeclaredOnly),
+                        typeof(ComponentModBase).GetMethod(nameof(ComponentModBase.Tick), BindingFlags.Instance | BindingFlags.Public | BindingFlags.DeclaredOnly)
+                    }
+                }
+                );
+        }
+
+        public Type DefineNewType(TLegacyComp legacy)
+        {
+            TypeBuilder typeBuilder = module.DefineType(typeof(TLegacyComp).Name + "_Compat", TypeAttributes.Public | TypeAttributes.Class, typeof(TComp));
+
+            // static field for storing the legacy ComponentModBase Instance
+            FieldBuilder legacyField = typeBuilder.DefineField(
+                    "_legacy",
+                    typeof(TLegacyComp),
+                    FieldAttributes.Private | FieldAttributes.Static);
+
+            // constructor taking SubType, Level, SubtypeData
+            ConstructorBuilder constructor = typeBuilder.DefineConstructor(MethodAttributes.Public, CallingConventions.Standard, new Type[] { typeof(int), typeof(int), typeof(short) });
+            ILGenerator constructorGenerator = constructor.GetILGenerator();
+            componentBaseConstructorBuilder(constructorGenerator);
+
+            // Define legacy interface
+            typeBuilder.AddInterfaceImplementation(typeof(ILegacyComponent));
+
+            // Define GetLegacyType method
+            MethodBuilder methodBuilder = typeBuilder.DefineMethod(nameof(ILegacyComponent.GetLegacyType), MethodAttributes.Public | MethodAttributes.Virtual, typeof(Type), Type.EmptyTypes);
+            ILGenerator il = methodBuilder.GetILGenerator();
+            il.Emit(OpCodes.Ldsfld, legacyField);
+            il.Emit(OpCodes.Call, objectGetType);
+            il.Emit(OpCodes.Ret);
+
+            typeBuilder.DefineMethodOverride(methodBuilder,typeof(ILegacyComponent).GetMethod(nameof(ILegacyComponent.GetLegacyType)));
+
+            // Define GetComponentMod method
+            methodBuilder = typeBuilder.DefineMethod(nameof(ILegacyComponent.GetComponentMod), MethodAttributes.Public | MethodAttributes.Virtual, typeof(ComponentModBase), Type.EmptyTypes);
+            il = methodBuilder.GetILGenerator();
+            il.Emit(OpCodes.Ldsfld, legacyField);
+            il.Emit(OpCodes.Castclass, typeof(ComponentModBase));
+            il.Emit(OpCodes.Ret);
+
+            typeBuilder.DefineMethodOverride(methodBuilder, typeof(ILegacyComponent).GetMethod(nameof(ILegacyComponent.GetComponentMod)));
+
+            // Forward ComponentModBaseMethods to base virtual methods of PLShipComponent / PLWare
+            foreach (var kvp in componentModBaseMethodOverrides)
+            {
+                DefineMethodForwarder(
+                    typeBuilder,
+                    legacyField,
+                    kvp.Key,
+                    kvp.Value
+                    );
+            }
+
+            //DefineMethodForwarders for methods not present in the base ComponentModBase class
+            nonComponentBaseMethodAddition(typeBuilder, legacyField);
+
+            Type generatedType = typeBuilder.CreateType();
+
+            generatedType.GetField("_legacy").SetValue(null, legacy);
+            return generatedType;
+        }
+
+        public static void DefineMethodForwarder(TypeBuilder typeBuilder, FieldInfo legacyField, MethodInfo targetMethod, MethodInfo legacyMethod)
+        {
+            ParameterInfo[] parameters = targetMethod.GetParameters();
+
+            Type[] paramTypes = Array.ConvertAll(parameters, p => p.ParameterType);
+
+            MethodBuilder method = typeBuilder.DefineMethod(
+                    targetMethod.Name,
+                    MethodAttributes.Public | MethodAttributes.ReuseSlot | MethodAttributes.Virtual | MethodAttributes.HideBySig,
+                    targetMethod.ReturnType,
+                    paramTypes
+                );
+
+            ILGenerator il = method.GetILGenerator();
+
+            // Load this instance
+            il.Emit(OpCodes.Ldarg_0);
+
+            // Call Closest Base Method
+            MethodInfo? closestMethodInfo = GetClosestParentMethod(typeBuilder.BaseType, targetMethod.Name);
+            if (closestMethodInfo is not null)
+            {
+                il.Emit(OpCodes.Call, closestMethodInfo);
+            }
+            else
+            {
+                il.Emit(OpCodes.Call, targetMethod);
+            }
+
+
+            // Load static legacy instance
+            il.Emit(OpCodes.Ldsfld, legacyField);
+
+            // Loads this instance
+            il.Emit(OpCodes.Ldarg_0);
+
+            // Load all parameters
+            for (short i = 0; i < paramTypes.Length; i++)
+            {
+                switch (i + 1)
+                {
+                    case 1: il.Emit(OpCodes.Ldarg_1); break;
+                    case 2: il.Emit(OpCodes.Ldarg_2); break;
+                    case 3: il.Emit(OpCodes.Ldarg_3); break;
+                    default:
+                        il.Emit(OpCodes.Ldarg_S, i + 1);
+                        break;
+                }
+            }
+
+            // Call legacy method
+            il.Emit(OpCodes.Callvirt, legacyMethod);
+
+            il.Emit(OpCodes.Ret);
+
+            typeBuilder.DefineMethodOverride(
+                method,
+                targetMethod);
+        }
+        public static MethodInfo? GetClosestParentMethod(Type subClassType, string methodName)
+        {
+            Type currentType = subClassType.BaseType;
+
+            while (currentType != null)
+            {
+                // Search for the method declared ONLY in the current base type being checked
+                var method = currentType.GetMethod(
+                    methodName,
+                    BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.DeclaredOnly
+                );
+
+                // If a method is found and is virtual/overridden, return it
+                if (method != null && (method.IsVirtual || method.IsAbstract))
+                {
+                    return method;
+                }
+
+                currentType = currentType.BaseType;
+            }
+
+            return null;
+        }
+    }
+}
