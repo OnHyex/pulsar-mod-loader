aff73ccd2b3622f9da76bd98914393347e5e4b12
OnHyex <100549656+OnHyex@users.noreply.github.com>
2026-06-03 02:56:31 -0400

    Updated ShieldModManager to fully working state based off of abstract ComponentModManager

--- PulsarModLoader/Content/Components/Shield/ShieldModManager.cs
+++ PulsarModLoader/Content/Components/Shield/ShieldModManager.cs
@@ -4,12 +4,12 @@ using System;
 using System.Collections.Generic;
 using System.Reflection;
 using System.Reflection.Emit;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using Logger = PulsarModLoader.Utilities.Logger;
 
 namespace PulsarModLoader.Content.Components.Shield
 {
-#warning "not complete inheritance done and CreateShield should be reviewed / maybe override the base constructor stuff then decide on constructor pattern"
     public class ShieldModManager : ComponentModManager<PLShieldGenerator,ShieldMod,EShieldGeneratorType>
     {
         public readonly int VanillaShieldMaxType = 0;
@@ -25,6 +25,11 @@ namespace PulsarModLoader.Content.Components.Shield
                 }
                 return m_instance;
             }
+        }
+        public ShieldModManager() : base((int)ESlotType.E_COMP_SHLD)
+        {
+            ShieldTypes = legacyModComps;
+            VanillaShieldMaxType = VanillaMaxType;
         }
         /// <summary>
         /// Finds Shield type equivilent to given name and returns Subtype ID needed to spawn. Returns -1 if couldn't find Shield.
@@ -58,96 +63,77 @@ namespace PulsarModLoader.Content.Components.Shield
             il.Emit(OpCodes.Ldarg_1);
             il.Emit(OpCodes.Ldarg_2);
             il.Emit(OpCodes.Call, constructor);
+            il.Emit(OpCodes.Ret);
+        }
+        //No Methods extra
+        protected override void ModComponentSubtypeMethods(Dictionary<MethodInfo, MethodInfo> methodOverrides)
+        {
+            return;
         }
         public static PLShieldGenerator CreateShield(int Subtype, int level)
         {
-            PLShieldGenerator InShield;
-            if (Subtype >= Instance.VanillaShieldMaxType)
+            return CreateShield(Subtype, level, 0);
+        }
+        public static PLShieldGenerator CreateShield(int Subtype, int level, int Subtypedata)
+        {
+            PLShieldGenerator comp;
+            if (Subtype >= Instance.VanillaMaxType)
             {
-                InShield = new PLShieldGenerator(EShieldGeneratorType.E_SG_ID_MAX, level);
-                int subtypeformodded = Subtype - Instance.VanillaShieldMaxType;
-                if (subtypeformodded <= Instance.ShieldTypes.Count && subtypeformodded > -1)
-                {
-                    ShieldMod ShieldType = Instance.ShieldTypes[Subtype - Instance.VanillaShieldMaxType];
-                    InShield.SubType = Subtype;
-                    InShield.Name = ShieldType.Name;
-                    InShield.Desc = ShieldType.Description;
-                    InShield.m_IconTexture = ShieldType.IconTexture;
-                    InShield.Max = ShieldType.ShieldMax;
-                    InShield.ChargeRateMax = ShieldType.ChargeRateMax;
-                    InShield.RecoveryRate = ShieldType.RecoveryRate;
-                    InShield.Deflection = ShieldType.Deflection;
-                    InShield.MinIntegrityPercentForQuantumShield = ShieldType.MinIntegrityPercentForQuantumShield;
-                    InShield.MinIntegrityAfterDamage = ShieldType.MinIntegrityAfterDamage;
-                    InShield.m_MaxPowerUsage_Watts = (ShieldType.MaxPowerUsage_Watts * 1.4f);
-                    InShield.m_MarketPrice = ShieldType.MarketPrice;
-                    InShield.CargoVisualPrefabID = ShieldType.CargoVisualID;
-                    InShield.CanBeDroppedOnShipDeath = ShieldType.CanBeDroppedOnShipDeath;
-                    InShield.Experimental = ShieldType.Experimental;
-                    InShield.Unstable = ShieldType.Unstable;
-                    InShield.Contraband = ShieldType.Contraband;
-                    InShield.Price_LevelMultiplierExponent = ShieldType.Price_LevelMultiplierExponent;
-                    if (InShield.MinIntegrityAfterDamage == -1)
-                    {
-                        InShield.MinIntegrityAfterDamage = Mathf.RoundToInt(InShield.Max * 0.15f);
-                    }
-                    InShield.MinIntegrityAfterDamage = Mathf.RoundToInt(InShield.MinIntegrityAfterDamage * (1f - Mathf.Clamp(0.05f * InShield.Level, 0f, 0.8f)));
-                    InShield.CurrentMax = InShield.Max;
-                    InShield.Current = InShield.Max;
-                }
+                comp = Instance.CreateComponent(Subtype - Instance.VanillaMaxType, level, Subtypedata);
+                comp.SubType = Subtype;
             }
             else
             {
-                InShield = new PLShieldGenerator((EShieldGeneratorType)Subtype, level);
+                comp = new PLShieldGenerator((EShieldGeneratorType)Subtype, level);
             }
-            return InShield;
+            return comp;
         }
     }
     //Converts hashes to Shields.
     [HarmonyPatch(typeof(PLShieldGenerator), "CreateShieldGeneratorFromHash")]
     class ShieldHashFix
     {
-        static bool Prefix(int inSubType, int inLevel, ref PLShipComponent __result)
+        static bool Prefix(int inSubType, int inLevel, int inSubTypeData, ref PLShipComponent __result)
         {
-            __result = ShieldModManager.CreateShield(inSubType, inLevel);
+            __result = ShieldModManager.CreateShield(inSubType, inLevel, inSubTypeData);
             return false;
         }
     }
     //Applies the Tick of the modded shields
-    [HarmonyPatch(typeof(PLShieldGenerator), "Tick")]
-    class TickPatch
-    {
-        static void Postfix(PLShieldGenerator __instance)
-        {
-            int subtypeformodded = __instance.SubType - ShieldModManager.Instance.VanillaShieldMaxType;
-            if (subtypeformodded > -1 && subtypeformodded < ShieldModManager.Instance.ShieldTypes.Count && __instance.ShipStats != null)
-            {
-                ShieldModManager.Instance.ShieldTypes[subtypeformodded].Tick(__instance);
-            }
-        }
-    }
-    [HarmonyPatch(typeof(PLShieldGenerator), "GetStatLineLeft")]
-    class LeftDescFix
-    {
-        static void Postfix(PLShieldGenerator __instance, ref string __result)
-        {
-            int subtypeformodded = __instance.SubType - ShieldModManager.Instance.VanillaShieldMaxType;
-            if (subtypeformodded > -1 && subtypeformodded < ShieldModManager.Instance.ShieldTypes.Count && __instance.ShipStats != null)
-            {
-                __result = ShieldModManager.Instance.ShieldTypes[subtypeformodded].GetStatLineLeft(__instance);
-            }
-        }
-    }
-    [HarmonyPatch(typeof(PLShieldGenerator), "GetStatLineRight")]
-    class RightDescFix
-    {
-        static void Postfix(PLShieldGenerator __instance, ref string __result)
-        {
-            int subtypeformodded = __instance.SubType - ShieldModManager.Instance.VanillaShieldMaxType;
-            if (subtypeformodded > -1 && subtypeformodded < ShieldModManager.Instance.ShieldTypes.Count && __instance.ShipStats != null)
-            {
-                __result = ShieldModManager.Instance.ShieldTypes[subtypeformodded].GetStatLineRight(__instance);
-            }
-        }
-    }
+    //[HarmonyPatch(typeof(PLShieldGenerator), "Tick")]
+    //class TickPatch
+    //{
+    //    static void Postfix(PLShieldGenerator __instance)
+    //    {
+    //        int subtypeformodded = __instance.SubType - ShieldModManager.Instance.VanillaShieldMaxType;
+    //        if (subtypeformodded > -1 && subtypeformodded < ShieldModManager.Instance.ShieldTypes.Count && __instance.ShipStats != null)
+    //        {
+    //            ShieldModManager.Instance.ShieldTypes[subtypeformodded].Tick(__instance);
+    //        }
+    //    }
+    //}
+    //[HarmonyPatch(typeof(PLShieldGenerator), "GetStatLineLeft")]
+    //class LeftDescFix
+    //{
+    //    static void Postfix(PLShieldGenerator __instance, ref string __result)
+    //    {
+    //        int subtypeformodded = __instance.SubType - ShieldModManager.Instance.VanillaShieldMaxType;
+    //        if (subtypeformodded > -1 && subtypeformodded < ShieldModManager.Instance.ShieldTypes.Count && __instance.ShipStats != null)
+    //        {
+    //            __result = ShieldModManager.Instance.ShieldTypes[subtypeformodded].GetStatLineLeft(__instance);
+    //        }
+    //    }
+    //}
+    //[HarmonyPatch(typeof(PLShieldGenerator), "GetStatLineRight")]
+    //class RightDescFix
+    //{
+    //    static void Postfix(PLShieldGenerator __instance, ref string __result)
+    //    {
+    //        int subtypeformodded = __instance.SubType - ShieldModManager.Instance.VanillaShieldMaxType;
+    //        if (subtypeformodded > -1 && subtypeformodded < ShieldModManager.Instance.ShieldTypes.Count && __instance.ShipStats != null)
+    //        {
+    //            __result = ShieldModManager.Instance.ShieldTypes[subtypeformodded].GetStatLineRight(__instance);
+    //        }
+    //    }
+    //}
 }
